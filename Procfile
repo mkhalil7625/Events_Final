eec004cb537cfbedf522e2998a20d31d27b5bc6f
mkhalil7625@gmail.com <mkhalil7625@gmail.com>
2019-04-20 03:42:23 -0500

    Initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Events_Final.wsgi
---
+++ events/views.py
@@ -0,0 +1,43 @@
+from .API.eventful_events import get_event
+from .database import db_config
+
+# import json
+# Create your views here.
+
+def searchResult(x):
+    # query = None
+    # if 'q' in request.GET:
+    #     query = request.GET.get('q')
+    #     print(query)
+    r = get_event(x)
+    print(x)
+    events_data = []
+    for event in r:
+        if event.get('performers') is not None:
+            events={'show_id':event['id'],
+                    # 'show' : event['title'],
+                'artist':event['performers']['performer']['name'],
+                'venue': event['venue_name'],
+                'city':event['city_name'],
+                'state':event['region_abbr'],
+                'date': event['start_time']}
+                # 'image_url': event["image"],
+        events_data.append(events)
+        print(events_data)
+
+        return events_data
+
+def load_events():
+    r=searchResult('first avenue')
+    for event in r:
+        artist = event.get('artist')
+        venue_name = event['venue']
+        city = event['city']
+        state = event['state']
+        show_id = event['show_id']
+        date = event['date']
+        print(artist)
+        db_config.add_record(artist,venue_name,city,state,show_id,date)
+
+
+load_events()
